6ce3024c216370d7940750ea73ffa72e002c847a
KadirToptas <kdrrtptss@gmail.com>
2024-06-17 22:24:48 +0300

    Project End

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -1,17 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArrowController : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    }
-    
+    [SerializeField] private Rigidbody2D rb;
     void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
+        Vector2 direction = rb.velocity;
+
+        float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
         
+        transform.rotation = Quaternion.AngleAxis(angle,Vector3.forward);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Target")
+        {
+            rb.isKinematic = true;
+            rb.velocity = Vector2.zero;
+        }
     }
 }
--- Assets/Scripts/BowController.cs
+++ Assets/Scripts/BowController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BowController : MonoBehaviour
 {
     private Vector2 _direction;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private float speed;
    
     void Update()
     {
@@ -13,5 +15,16 @@ public class BowController : MonoBehaviour
 
         _direction = mousePos - bowPosition;
         transform.right = _direction;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject newArrow = Instantiate(arrowPrefab, transform.position, transform.rotation);
+        newArrow.GetComponent<Rigidbody2D>().AddForce(transform.right*speed);
     }
 }
